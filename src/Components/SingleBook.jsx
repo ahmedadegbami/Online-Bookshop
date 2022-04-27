e9a10623433d7705304247352bf930eb160e361e
ahmedadegbami <ahmed.adegbami@gmail.com>
2022-04-27 19:18:44 +0200

    Update SingleBook.jsx

--- src/Components/SingleBook.jsx
+++ src/Components/SingleBook.jsx
@@ -2,36 +2,33 @@ import { Component } from "react";
 import { Card } from "react-bootstrap";
 // import CommentArea from "./CommentArea";
 
-class SingleBook extends Component {
+const SingleBook = (props) => {
   // state = {
   //   selected: false,
   // };
-  render() {
-    return (
-      <>
-        <Card>
-          <Card.Img
-            variant="top"
-            src={this.props.book.img}
-            // value={this.state.selected}
-            // onClick={() =>
-            //   this.setState({
-            //     selected: !this.state.selected,
-            //   })
-            onClick={() => this.props.changeSelectedBook(this.props.book.asin)}
-            // style={{ border: this.state.selected ? "2px solid red " : "none" }}
-          />
-          <Card.Body>
-            <Card.Title className="text-dark">
-              {this.props.book.title}
-            </Card.Title>
-          </Card.Body>
-        </Card>
 
-        {/* {this.state.selected && <CommentArea asin={this.props.book.asin} />} */}
-      </>
-    );
-  }
-}
+  return (
+    <>
+      <Card>
+        <Card.Img
+          variant="top"
+          src={props.book.img}
+          // value={this.state.selected}
+          // onClick={() =>
+          //   this.setState({
+          //     selected: !this.state.selected,
+          //   })
+          onClick={() => props.changeSelectedBook(props.book.asin)}
+          // style={{ border: this.state.selected ? "2px solid red " : "none" }}
+        />
+        <Card.Body>
+          <Card.Title className="text-dark">{props.book.title}</Card.Title>
+        </Card.Body>
+      </Card>
+
+      {/* {this.state.selected && <CommentArea asin={this.props.book.asin} />} */}
+    </>
+  );
+};
 
 export default SingleBook;
